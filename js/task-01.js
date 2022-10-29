0f047c149fc997969f836c35c5f756b4abde0b8c
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-10-30 01:56:10 +0300

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const categories = document.querySelector("ul#categories");
+
+const numberOfCategories = categories.querySelectorAll("li.item").length;
+console.log("Number of categories:", numberOfCategories);
+
+const infoOfCategory = categories.querySelectorAll("li.item").forEach((item) => {
+   console.log("Category:", item.querySelector("h2").textContent);
+   console.log("Elements:", item.querySelectorAll("li").length);
+});
+
+
+
+
+
