77de2608bd4b97cb5f256c68cf8d175908c70f7a
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-11-21 00:58:34 +0200

    task-4

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+
+const decrementBtnRef = document.querySelector('[data-action="decrement"]');
+const incrementBtnRef = document.querySelector('[data-action="increment"]');
+const valueRef = document.querySelector('#value');
+
+decrementBtnRef.addEventListener("click", onDecrementBtnClick);
+incrementBtnRef.addEventListener("click", onIncrementBtnClick);
+
+function onDecrementBtnClick() {
+    counterValue -= 1;
+    valueRef.textContent = counterValue;
+}
+
+function onIncrementBtnClick() {
+    counterValue += 1;
+    valueRef.textContent = counterValue;
+}
