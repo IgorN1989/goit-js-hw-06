02ef963f73178bd2f16a3b501ab9103fd8143d5a
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-11-21 01:19:06 +0200

    task-5

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", onInput);
+
+function onInput(evt) {
+    if (evt.currentTarget.value.length > 0) {
+        outputRef.textContent = evt.currentTarget.value;
+    } else {
+        outputRef.textContent = "Anonymous";
+    }
+};
+
