01dbd3f10b7e5aa5b7bcc005aabd5ca6b00e6080
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2022-11-22 00:09:57 +0200

    update Task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,13 @@
-const inputRef = document.querySelector("#validation-input");
+const inputRef = document.querySelector('#validation-input');
+const dataLengthRef = Number(inputRef.getAttribute('data-length'));
 
-inputRef.addEventListener("blur", onInputCheck);
+inputRef.addEventListener('blur', onInputCheck);
 
 function onInputCheck(evt) {
-    if (evt.currentTarget.value.length === 6) {
-        evt.currentTarget.classList.add("valid");
-        evt.currentTarget.classList.remove("invalid");
-    } else {
-        evt.currentTarget.classList.add("invalid");
-    }
+  if (evt.currentTarget.value.length === dataLengthRef) {
+    evt.currentTarget.classList.add('valid');
+    evt.currentTarget.classList.remove('invalid');
+  } else {
+    evt.currentTarget.classList.add('invalid');
+  }
 }
